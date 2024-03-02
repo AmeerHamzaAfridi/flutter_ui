3bc3233311a9cc143c4f837174ceacd6f61b8b31
AmeerHamzaAfridi <hxmzadev@gmail.com>
2024-03-02 08:03:13 +0500

    Day2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Title'),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text(
+          '30 Days Flutter UI Practice',
+          style: TextStyle(color: Colors.white),
         ),
-        body: Column(
-          children: [Text('This is me')],
-        ));
+      ),
+      body: const Column(
+        children: [Text('This is me')],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.white,
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              customButton('Home', Icons.home),
+              const SizedBox(
+                height: 20,
+              ),
+              customButton('Information', Icons.info),
+              const SizedBox(
+                height: 20,
+              ),
+              customButton('Contact Us', Icons.contact_emergency),
+              const SizedBox(
+                height: 20,
+              ),
+              customButton('LogOut', Icons.logout_outlined),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  ElevatedButton customButton(String name, IconData icon) {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Row(
+        children: [
+          Text(
+            name,
+            style: const TextStyle(color: Colors.black),
+          ),
+          const SizedBox(
+            width: 20,
+          ),
+          Icon(
+            icon,
+            color: Colors.black,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      title: '30 Days Flutter UI Practice',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: const HomePage(),
     );
   }
 }
-
-
